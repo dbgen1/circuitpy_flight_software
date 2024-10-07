a98847e291a947b90b395fa687ebe75a66a34a6c
davit <davitb2013@gmail.com>
2024-10-07 13:00:14 -0700

    make command eval safe

--- FC_Board/cdh.py
+++ FC_Board/cdh.py
@@ -14,16 +14,6 @@ jokereply = [
 # pass-code for DEMO PURPOSES ONLY
 super_secret_code = b""  # put your own code here
 print(f"Super secret code is: {super_secret_code}")
-commands = {
-    b"\x8eb": "noop",
-    b"\xd4\x9f": "hreset",  # new
-    b"\x12\x06": "shutdown",
-    b"8\x93": "query",  # new
-    b"\x96\xa2": "exec_cmd",
-    b"\xa5\xb4": "joke_reply",
-    b"\x56\xc4": "FSK",
-}
-
 
 ############### hot start helper ###############
 def hotstart_handler(cubesat, msg):
@@ -46,60 +36,6 @@ def hotstart_handler(cubesat, msg):
         print(
             f"not for me? target id: {hex(msg[0])}, my id: {hex(cubesat.radio1.node)}"
         )
-
-
-############### message handler ###############
-def message_handler(cubesat, msg):
-    multi_msg = False
-    if len(msg) >= 10:  # [RH header 4 bytes] [pass-code(4 bytes)] [cmd 2 bytes]
-        if bytes(msg[4:8]) == super_secret_code:
-            # check if multi-message flag is set
-            if msg[3] & 0x08:
-                multi_msg = True
-            # strip off RH header
-            msg = bytes(msg[4:])
-            cmd = msg[4:6]  # [pass-code(4 bytes)] [cmd 2 bytes] [args]
-            cmd_args = None
-            if len(msg) > 6:
-                print("command with args")
-                try:
-                    cmd_args = msg[6:]  # arguments are everything after
-                    print("cmd args: {}".format(cmd_args))
-                except Exception as e:
-                    print("arg decoding error: {}".format(e))
-            if cmd in commands:
-                try:
-                    if cmd_args is None:
-                        print("running {} (no args)".format(commands[cmd]))
-                        # eval a string turns it into a func name
-                        eval(commands[cmd])(cubesat)
-                    else:
-                        print(
-                            "running {} (with args: {})".format(commands[cmd], cmd_args)
-                        )
-                        eval(commands[cmd])(cubesat, cmd_args)
-                except Exception as e:
-                    print("something went wrong: {}".format(e))
-                    cubesat.radio1.send(str(e).encode())
-            else:
-                print("invalid command!")
-                cubesat.radio1.send(b"invalid cmd" + msg[4:])
-            # check for multi-message mode
-            if multi_msg:
-                # TODO check for optional radio config
-                print("multi-message mode enabled")
-                response = cubesat.radio1.receive(
-                    keep_listening=True,
-                    with_ack=True,
-                    with_header=True,
-                    view=True,
-                    timeout=10,
-                )
-                if response is not None:
-                    cubesat.c_gs_resp += 1
-                    message_handler(cubesat, response)
-        else:
-            print("bad code?")
 
 
 ########### commands without arguments ###########
@@ -129,8 +65,6 @@ def joke_reply(cubesat):
 
 
 ########### commands with arguments ###########
-
-
 def shutdown(cubesat, args):
     # make shutdown require yet another pass-code
     if args == b"\x0b\xfdI\xec":
@@ -170,3 +104,68 @@ def query(cubesat, args):
 def exec_cmd(cubesat, args):
     print(f"exec: {args}")
     exec(args)
+
+############### Command List ###############
+commands = {
+    b"\x8eb": noop,
+    b"\xd4\x9f": hreset,  # new
+    b"\x12\x06": shutdown,
+    b"8\x93": query,  # new
+    b"\x96\xa2": exec_cmd,
+    b"\xa5\xb4": joke_reply,
+    b"\x56\xc4": FSK
+}
+
+
+############### message handler ###############
+def message_handler(cubesat, msg):
+    multi_msg = False
+    if len(msg) >= 10:  # [RH header 4 bytes] [pass-code(4 bytes)] [cmd 2 bytes]
+        if bytes(msg[4:8]) == super_secret_code:
+            # check if multi-message flag is set
+            if msg[3] & 0x08:
+                multi_msg = True
+            # strip off RH header
+            msg = bytes(msg[4:])
+            cmd = msg[4:6]  # [pass-code(4 bytes)] [cmd 2 bytes] [args]
+            cmd_args = None
+            if len(msg) > 6:
+                print("command with args")
+                try:
+                    cmd_args = msg[6:]  # arguments are everything after
+                    print("cmd args: {}".format(cmd_args))
+                except Exception as e:
+                    print("arg decoding error: {}".format(e))
+            if cmd in commands:
+                try:
+                    if cmd_args is None:
+                        print("running {} (no args)".format(commands[cmd]))
+                        # commands value directly refers to function object
+                        commands[cmd](cubesat)
+                    else:
+                        print(
+                            "running {} (with args: {})".format(commands[cmd], cmd_args)
+                        )
+                        commands[cmd](cubesat, cmd_args)
+                except Exception as e:
+                    print("something went wrong: {}".format(e))
+                    cubesat.radio1.send(str(e).encode())
+            else:
+                print("invalid command!")
+                cubesat.radio1.send(b"invalid cmd" + msg[4:])
+            # check for multi-message mode
+            if multi_msg:
+                # TODO check for optional radio config
+                print("multi-message mode enabled")
+                response = cubesat.radio1.receive(
+                    keep_listening=True,
+                    with_ack=True,
+                    with_header=True,
+                    view=True,
+                    timeout=10,
+                )
+                if response is not None:
+                    cubesat.c_gs_resp += 1
+                    message_handler(cubesat, response)
+        else:
+            print("bad code?")
